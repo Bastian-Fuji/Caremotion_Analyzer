815fba6772ccd07e4cb581b42cad87dc7836d946
kakeru58 <yamasaki586868@gmail.com>
2024-06-27 00:37:13 +0900

    [Try]AWS1

--- database.py
+++ database.py
@@ -1,35 +1,37 @@
 import os
-import boto3
 from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, Table
 from sqlalchemy.orm import sessionmaker
+import boto3
+import streamlit as st
 
-# Amazon S3認証
+# Amazon S3設定
 s3 = boto3.client(
     's3',
     aws_access_key_id=st.secrets["s3"]["aws_access_key_id"],
-    aws_secret_access_key=st.secrets["s3"]["aws_secret_access_key"]
+    aws_secret_access_key=st.secrets["s3"]["aws_secret_access_key"],
+    region_name=st.secrets["s3"]["region"]
 )
 BUCKET_NAME = st.secrets["s3"]["bucket_name"]
+
+# S3にファイルをアップロードする関数
+def upload_file_to_s3(local_path, s3_path=None):
+    if s3_path is None:
+        s3_path = os.path.basename(local_path)
+    s3.upload_file(local_path, BUCKET_NAME, s3_path)
+    return s3_path
 
 # 保存ディレクトリの設定
 SAVE_DIR = "uploaded_files"
 if not os.path.exists(SAVE_DIR):
     os.makedirs(SAVE_DIR)
 
-# S3にファイルをアップロードする関数
-def upload_file_to_s3(local_path, bucket_name, s3_path):
-    s3.upload_file(local_path, bucket_name, s3_path)
-    return f"s3://{bucket_name}/{s3_path}"
-
 # データベースディレクトリの設定
 DB_DIR = "CareMotionAnalyzer/DB"
 if not os.path.exists(DB_DIR):
     os.makedirs(DB_DIR)
 
-DATABASE_PATH = os.path.join(DB_DIR, 'uploaded_data.db')
-
 # データベース設定
-DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
+DATABASE_URL = f"sqlite:///{os.path.join(DB_DIR, 'uploaded_data.db')}"
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
 
@@ -52,6 +54,3 @@ uploads_table = Table(
 metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
-
-# データベースファイルをS3にアップロード
-s3_db_path = upload_file_to_s3(DATABASE_PATH, BUCKET_NAME, 'uploaded_data.db')
