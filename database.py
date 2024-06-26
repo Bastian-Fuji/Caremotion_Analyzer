629ade1be63685cb975f8999131b076f9af15408
kakeru58 <yamasaki586868@gmail.com>
2024-06-27 02:14:03 +0900

    [Try]AWS9

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, 
 from sqlalchemy.orm import sessionmaker
 import boto3
 import streamlit as st
+from botocore.exceptions import ClientError
 
 # Amazon S3設定
 s3 = boto3.client(
@@ -13,14 +14,27 @@ s3 = boto3.client(
 )
 BUCKET_NAME = st.secrets["s3"]["bucket_name"]
 
-# S3からファイルをダウンロードする関数
-def download_file_from_s3(s3_path, local_path):
-    s3.download_file(BUCKET_NAME, s3_path, local_path)
-
 # S3にファイルをアップロードする関数
-def upload_file_to_s3(local_path, s3_path):
+def upload_file_to_s3(local_path, s3_path=None):
+    if s3_path is None:
+        s3_path = os.path.join('DB', os.path.basename(local_path))
     s3.upload_file(local_path, BUCKET_NAME, s3_path)
     return s3_path
+
+# S3からファイルをダウンロードする関数
+def download_file_from_s3(s3_path, local_path=None):
+    if local_path is None:
+        local_path = os.path.join('DB', os.path.basename(s3_path))
+    try:
+        s3.download_file(BUCKET_NAME, s3_path, local_path)
+    except ClientError as e:
+        if e.response['Error']['Code'] == '404':
+            # ファイルが存在しない場合、新しいデータベースファイルを作成
+            with open(local_path, 'w'):
+                pass
+        else:
+            raise
+    return local_path
 
 # 保存ディレクトリの設定
 SAVE_DIR = "BVH"
@@ -34,7 +48,7 @@ if not os.path.exists(DB_DIR):
 
 DATABASE_PATH = os.path.join(DB_DIR, 'uploaded_data.db')
 
-# アプリケーション開始時にS3からデータベースファイルをダウンロード
+# S3からデータベースファイルをダウンロード
 download_file_from_s3('DB/uploaded_data.db', DATABASE_PATH)
 
 # データベース設定
